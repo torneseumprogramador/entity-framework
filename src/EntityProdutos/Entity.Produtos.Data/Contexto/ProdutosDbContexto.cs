3843697de905f4e20d1d3d68207a4ec65cde5a3a
Raphael Silvestre dos Santos <raphael.silvestre@animaeducacao.com.br>
2021-10-07 23:52:21 -0300

    Concluida separação de contextos

---
+++ src/EntityProdutos/Entity.Produtos.Data/Contexto/ProdutosDbContexto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Entity.Produtos.Data;
+
+#nullable disable
+
+namespace Entity.Produtos.Data.Contexto
+{
+    public partial class ProdutosDbContexto : DbContext
+    {
+        public ProdutosDbContexto()
+        {
+        }
+
+        public ProdutosDbContexto(DbContextOptions<ProdutosDbContexto> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Produto> Produtos { get; set; }
+        public virtual DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=localhost;database=EntityFrameworkComunidade;user=root;password=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.26-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasCharSet("utf8mb4")
+                .UseCollation("utf8mb4_0900_ai_ci");
+
+            modelBuilder.Entity<Produto>(entity =>
+            {
+                entity.ToTable("produtos");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Descricao)
+                    .HasColumnType("text")
+                    .HasColumnName("descricao");
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .HasColumnName("nome");
+
+                entity.Property(e => e.UrlImagem)
+                    .HasMaxLength(255)
+                    .HasColumnName("url_imagem");
+
+                entity.Property(e => e.Valor).HasColumnName("valor");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
